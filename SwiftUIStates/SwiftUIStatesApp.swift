a46ad2e4631c8d03a0c2e9fb77f94d93e13fd4ca
Paige Sun <paige.sun.dev@gmail.com>
2023-06-23 15:49:33 -0700

    init: Create empty SwiftUI Project

---
+++ SwiftUIStates/SwiftUIStatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStatesApp.swift
+//  SwiftUIStates
+//
+//  Created by Paige Sun on 2023-06-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
